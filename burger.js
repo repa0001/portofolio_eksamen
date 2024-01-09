81cc4b3208085b2f10e616cbfbd0da330888d5fd
Rebecca <repa0001@stud.kea.dk>
2024-01-09 18:24:59 +0100

    Portfolio eksamen

---
+++ burger.js
@@ -0,0 +1,16 @@
+// fået fra undervisningsmatriale
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".menu");
+const links = document.querySelectorAll(".menu li a");
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active");
+  nav.classList.toggle("active");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    burger.classList.remove("active");
+    nav.classList.remove("active");
+  });
+});
